2751950abc05a43d40d14ecc6d588fead50ff850
eunchu <lovely-ej01@nate.com>
2019-06-11 02:10:37 +0900

    Create use-title

---
+++ useTitle/useTitle.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @param {String} initialTitle 
+ */
+export const useTitle = initialTitle => {
+  const [title, setTitle] = useState(initialTitle);
+
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+
+  return setTitle;
+};
